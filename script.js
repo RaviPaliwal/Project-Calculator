a8eb968fb39602855d5918c3261805655c0e1a40
RaviPaliwal <112271564+RaviPaliwal@users.noreply.github.com>
2022-08-27 23:17:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function display values
+function display(value){
+  
+    if(document.getElementById("result").value == '0')
+    { //if zero preceding then eval throwing error
+      var k = value;
+      clearScreen();
+      document.getElementById("result").value +=value;
+    }
+    //why without this we have to press a button 2 times after zero
+    else if(dis[0].style.backgroundColor =='black')
+   {
+      clearScreen();
+    }
+  else
+   {
+    document.getElementById("result").value += value;
+   }
+} 
+
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+ }
+
+function changeColor(id)
+{
+  document.getElementById(id).style.backgroundColor = "#ff0000"; // backcolor
+  
+}
+
+
+
+var dis = document.getElementsByClassName('display-box');
+function onf() {
+  if (dis[0].style.backgroundColor !='greenyellow')
+  {
+    console.log('Turning on')
+    dis[0].style.backgroundColor = 'greenyellow';
+  }
+  else
+  {
+    console.log('Turning off')
+    dis[0].style.backgroundColor = 'black';
+    clearScreen();
+  }
+}
